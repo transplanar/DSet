3506bd2cf97c7d416cb04864bd10eb99f6a18420
transplanar <glen@glencooney.com>
2016-01-16 21:13:57 -0500

    Correct syntax error in home_controller specs

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -7,23 +7,19 @@ RSpec.describe HomeController, type: :controller do
       load Rails.root + "db/seeds.rb"
     end
 
-    # it 'renders index template' do
-    #   get :index
-    #
-    #   expect(response).to render_template("index")
-    # end
-
     # FIXME move this to a better spot
       # REVIEW move this to different controller?
     it 'returns correct number of search matches for ALPHABETICAL input' do
-      get :index, {search: "V"}
+      # get :index, {search: "V"}
+      get :index, {search: "V"}, format: :js
       cards = assigns(:cards)
 
       expect(cards.count).to eq(3)
     end
 
     it 'returns correct number of search matches for NUMERIC input' do
-      get :index, {search: 2}
+      # get :index, {search: 2}
+      get :index, {search: 2}, format: :js
       cards = assigns(:cards)
 
       expect(cards.count).to eq(3)
@@ -32,12 +28,12 @@ RSpec.describe HomeController, type: :controller do
   end
 
   # FIXME delete this
-  describe 'Home#about' do
+  # describe 'Home#about' do
     # it 'renders about template' do
     #   get :about
     #
     #   expect(response).to render_template("about")
     # end
-  end
+  # end
 
 end
