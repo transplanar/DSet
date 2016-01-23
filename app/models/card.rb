0555f038fcd86a95c0e3d501924857c2fe6a8bb8
transplanar <glen@glencooney.com>
2016-01-22 22:06:17 -0500

    More progress on search parsing.

--- app/models/card.rb
+++ app/models/card.rb
@@ -15,12 +15,30 @@ class Card < ActiveRecord::Base
       @cards = []
       @matched_terms = []
 
+
       search_queries.each do |query|
         columns.each do |col|
           if use_fuzzy_search && !is_numeric?(query)
             cards = Card.send("find_by_fuzzy_#{col}", query)
           else
-            cards = Card.where("#{col} LIKE ?","%#{query}%")
+            if @results.blank?
+              cards = Card.where("#{col} LIKE ?","%#{query}%")
+            else
+              cards = Card.where("#{col} LIKE ?","%#{query}%")
+
+              # puts @results.where("#{col} LIKE ?","%#{query}%")
+              # puts Card.where(@results[col].map(&:card_id).uniq)
+
+              # @results.where("#{col} LIKE ?","%#{query}%")
+
+              # puts "TEST #{ @results.select{ |c| c["#{col}"] == query}  } "
+
+
+              # cards = Card.where("#{col} LIKE ?","%#{query}%")
+
+              # cards = @results.select(|card|           )
+              # cards = Card.where(@results[col].map(&:col).uniq)
+            end
           end
 
           unless cards.empty?
@@ -32,16 +50,15 @@ class Card < ActiveRecord::Base
                 split_terms = c["#{col}"].split(',')
 
                 split_terms.each do |term|
-                  puts "#{term} vs #{query} is #{term.include? query}"
+                  # puts "#{term} vs #{query} is #{term.include? query}"
                   if term.downcase.include? query.downcase
                     @matched_terms << "<b>#{col}</b>: #{term}"
-                    # @matched_terms << term
                     @matched_terms.uniq!
                   end
                 end
               end
             end
-            puts "Matched terms array #{@matched_terms}"
+            # puts "Matched terms array #{@matched_terms}"
           end
         end
       end
@@ -79,17 +96,23 @@ class Card < ActiveRecord::Base
       # XXX FOR TESTING
       # puts ">>>>>>>Results #{@results}"
       #
-      # @results.each do |k,v|
-      #   v.each do |card|
-      #     p "Card = #{card[:name]} in #{k}"
-      #   end
-      # end
+      @results.each do |k,v|
+        v.each do |card|
+          p "Card = #{card[:name]} in #{k}"
+        end
+      end
+
+      # puts "FLATTEN 1x #{@results.flatten(1)}"
+      # puts "FLATTEN 2x #{@results.flatten}"
+      # puts "TEST #{ @results.select{ |c| c["name"] == query}  } "
       #>>>>>>>>>>>>>>>>>>>>>>>>>
 
     else
       @results = {}
       @matched_terms = {}
     end
+
+
 
     return [@results, @matched_terms]
   end
