3d1f1eb938539f72a900d65c465e88430749dfc6
transplanar <glen@glencooney.com>
2016-01-26 19:18:26 -0500

    Trying to incorporate complex SQL and ARel

--- app/models/card.rb
+++ app/models/card.rb
@@ -14,122 +14,187 @@ class Card < ActiveRecord::Base
 
   def self.search search, use_fuzzy_search
     unless search.blank?
-
-      # exclude_columns = ['id', 'image_url', 'created_at', 'updated_at']
       exclude_columns = ['id', 'image_url', 'created_at', 'updated_at', 'slot_id']
       columns = Card.attribute_names - exclude_columns
 
+      # FIXME if you have a query ending in a comma w/o a space, gives bad results
       search_queries = search.split(', ')
 
-      # if search_queries.count > 1
-      #   @results
-      #   @multisearch = true
-      # else
-      #   @results = Hash.new
-      #   @multisearch = false
-      # end
-      #
-      # XXX set true for testing
-      @multisearch = true
+      @multisearch = search_queries.count > 1
+      puts "MULTISEARCH #{@multisearch}"
 
       @cards = []
       t_results = []
 
-      @matched_terms = []
-
-      column_match = 0
-
       sql_string = ''
 
-      t_cards = Arel::Table.new(:cards)
-      puts "TESTING AREL"
-      # puts Card.where(t_cards[:name]).matches("%v%")
-      # test = Card.where(t_cards[:name].matches("%v%")).where(t_cards[:cost].matches(3))
-
-      # inner = Card.where(cost: 5)
-      # outer = Card.where(cost: 5)
-      # inner = Card.where(category: "%v%")
-      # inner = Card.where(name: "%v%")
-      inner = Card._category('v')
-      # outer = Card.all
-      # outer = Card.where(cost: 3)
-      outer = Card._cost(5)
-      # outer = Card.all
-      # outer = outer.from(Arel.sql("(#{inner.to_sql}) as results")).select("*")
-      outer = outer.from(Arel.sql("(#{inner.to_sql})")).select("*")
-
-      outer.each do |k, v|
-        puts k.name
-      end
-      # puts outer
-      # User.where(users[:name].matches("%#{user_name}%"))
+      # match_columns = []
+      match_columns = Hash.new
 
       search_queries.each do |query|
         columns.each do |col|
-          if use_fuzzy_search && !is_numeric?(query)
-            cards = Card.send("find_by_fuzzy_#{col}", query)
-          else
-              # cards = Card.send( "_#{col}", query )
-
-              # TODO if results are in multiple category, append "OR" instead of "AND"
-              # IF a single query produces matches in multiple categories, use OR
-              if Card.send( "_#{col}", query ).count > 0
-                if query == search_queries.first
-                  sql_string = Card.send( "_#{col}", query ).to_sql
-                  column_match = column_match + 1
-                  puts "Column match updated to #{column_match}"
-                else
-                  # Needs to still split by category? Tries to find something that matches in NAME and CATEGORY rather than one or the other
-                  # sql_string = sql_string + " AND " + Card.send( "_#{col}", query ).to_sql.gsub(/.*?(?=\()/im, "")
-                  if column_match > 0
-                    sql_string = sql_string + " OR " + Card.send( "_#{col}", query ).to_sql.gsub(/.*?(?=\()/im, "")
-                  else
-                    sql_string = sql_string + " AND " + Card.send( "_#{col}", query ).to_sql.gsub(/.*?(?=\()/im, "")
-                  end
-
-                  # XXX template
-                  # SELECT COUNT(*) FROM "cards" WHERE (category LIKE '%village%') AND (cost LIKE '%5%') AND (slot_id LIKE '%5%')
-                  # needed
-                  # SELECT COUNT(*) FROM "cards" WHERE (category LIKE '%village%') AND (cost LIKE '%5%') AND (slot_id LIKE '%5%')
-                end
-              end
+          # If a match is found in a given column, add that to array of matched columns
+          unless Card.send( "_#{col}", query ).blank?
+            # XXX prevent this from being overridden in multi-search
+            if match_columns[col].blank?
+              match_columns[col] = query
+            else
+              match_columns[col] = match_columns[col] +", " + query
+            end
           end
+        end
+      end
 
-        #   unless cards.empty?
-        #   # unless cards.blank?
-        #     unless search_queries.count > 1
-        #       @results[col]  = cards
-        #     else
-        #       @results = cards
-        #       # @matched_terms = []
-        #     end
-        #
-        #     cards.each do |c|
-        #       unless col == "cost"
-        #         split_terms = c["#{col}"].split(', ')
-        #
-        #         split_terms.each do |term|
-        #           if term.downcase.include? query.downcase
-        #             @matched_terms << "<b>#{col}</b>: #{term}"
-        #             @matched_terms.uniq!
-        #           end
-        #         end
-        #       else
-        #         @matched_terms << "<b>#{col}</b>: #{c["#{col}"]}"
-        #         @matched_terms.uniq!
-        #       end
-        #     end
-        #   end
+      # table = Arel::Table.new(:cards)
+
+      puts "MATCH COLUMNS"
+      match_columns.each do |k, v|
+        puts "#{k}: #{v}"
+        # puts "Matched term: #{Card.send( "_#{k}", v).name}"
+        # puts "Matched terms:"
+        # Card.send( "_#{k}", v).each do |card|
+        #   puts card[k]
+        # end
+      end
+
+      unless @multisearch
+        query = search_queries
+        # Supports matches across multiple columns
+        match_columns.each do |col, query|
+          if sql_string.empty?
+            sql_string = Card.send( "_#{col}", query).to_sql
+          else
+            sql_string = sql_string + " OR " + Card.send( "_#{col}", query ).to_sql.gsub(/.*?(?=\()/im, "")
+          end
         end
 
-        # column_match = 0
+        unless sql_string.blank?
+          @results = Card.find_by_sql(sql_string)
+
+          puts "RAW SQL string = #{sql_string}"
+          puts "Results found #{@results.count}"
+        end
+      else
+
+        # search_queries.each do |q2|
+        #   match_columns.first do |col, q1|
+        #     # sql_string = Card.send()
+        #     Card.send( "_#{col}", q) + " AND "
+        #
+        #
+        #     # if sql_string.empty?
+        #     #   sql_string = Card.send( "_#{col.to_s}", query).to_sql
+        #     # else
+        #     #   sql_string = sql_string + " AND " + Card.send( "_#{col}", query ).to_sql.gsub(/.*?(?=\()/im, "")
+        #     # end
+        #   end
+        # end
+
+        # unless sql_string.blank?
+        #   @results = Card.find_by_sql(sql_string)
+        #
+        #   puts "RAW SQL string = #{sql_string}"
+        #   puts "Results found #{@results.count}"
+        # end
       end
 
-      unless sql_string.blank?
-        puts "RAW SQL string = #{sql_string}"
-        @results = Card.find_by_sql(sql_string)
-        puts "Results found #{@results.count}"
-      end
+      # Restore split results by column as searches may be down many branches?
+      # Render results in single SQL statement
+      # unless sql_string.blank?
+      #   @results = Card.find_by_sql(sql_string)
+      #
+      #   puts "RAW SQL string = #{sql_string}"
+      #   puts "Results found #{@results.count}"
+      # end
+
+
+
+
+
+      # outer = Card._types('action')
+      #
+      # # outer = Card.all
+      # # outer = Card.where(cost: 3)
+      #
+      # # outer = Card._cost(6)
+      # inner = Card._cost(6)
+      #
+      # # outer = Card.all
+      # # outer = outer.from(Arel.sql("(#{inner.to_sql}) as results")).select("*")
+      # outer = outer.from(Arel.sql("(#{inner.to_sql})")).select("*")
+      #
+      # outer.each do |c|
+      #   puts c.name
+      # end
+      # # puts outer
+      # # User.where(users[:name].matches("%#{user_name}%"))
+      #
+      # search_queries.each do |query|
+      #   columns.each do |col|
+      #     if use_fuzzy_search && !is_numeric?(query)
+      #       cards = Card.send("find_by_fuzzy_#{col}", query)
+      #     else
+      #         # cards = Card.send( "_#{col}", query )
+      #
+      #         # TODO if results are in multiple category, append "OR" instead of "AND"
+      #         # IF a single query produces matches in multiple categories, use OR
+      #         if Card.send( "_#{col}", query ).count > 0
+      #           if query == search_queries.first
+      #             sql_string = Card.send( "_#{col}", query ).to_sql
+      #             column_match = column_match + 1
+      #             puts "Column match updated to #{column_match}"
+      #           else
+      #             # Needs to still split by category? Tries to find something that matches in NAME and CATEGORY rather than one or the other
+      #             # sql_string = sql_string + " AND " + Card.send( "_#{col}", query ).to_sql.gsub(/.*?(?=\()/im, "")
+      #             if column_match > 0
+      #               sql_string = sql_string + " OR " + Card.send( "_#{col}", query ).to_sql.gsub(/.*?(?=\()/im, "")
+      #             else
+      #               sql_string = sql_string + " AND " + Card.send( "_#{col}", query ).to_sql.gsub(/.*?(?=\()/im, "")
+      #             end
+      #
+      #             # XXX template
+      #             # SELECT COUNT(*) FROM "cards" WHERE (category LIKE '%village%') AND (cost LIKE '%5%') AND (slot_id LIKE '%5%')
+      #             # needed
+      #             # SELECT COUNT(*) FROM "cards" WHERE (category LIKE '%village%') AND (cost LIKE '%5%') AND (slot_id LIKE '%5%')
+      #           end
+      #         end
+      #     end
+      #
+      #   #   unless cards.empty?
+      #   #   # unless cards.blank?
+      #   #     unless search_queries.count > 1
+      #   #       @results[col]  = cards
+      #   #     else
+      #   #       @results = cards
+      #   #       # @matched_terms = []
+      #   #     end
+      #   #
+      #   #     cards.each do |c|
+      #   #       unless col == "cost"
+      #   #         split_terms = c["#{col}"].split(', ')
+      #   #
+      #   #         split_terms.each do |term|
+      #   #           if term.downcase.include? query.downcase
+      #   #             @matched_terms << "<b>#{col}</b>: #{term}"
+      #   #             @matched_terms.uniq!
+      #   #           end
+      #   #         end
+      #   #       else
+      #   #         @matched_terms << "<b>#{col}</b>: #{c["#{col}"]}"
+      #   #         @matched_terms.uniq!
+      #   #       end
+      #   #     end
+      #   #   end
+      #   end
+      #
+      #   # column_match = 0
+      # end
+      #
+      # unless sql_string.blank?
+      #   puts "RAW SQL string = #{sql_string}"
+      #   @results = Card.find_by_sql(sql_string)
+      #   puts "Results found #{@results.count}"
+      # end
 
       # WORKING
       # columns.each do |col|
