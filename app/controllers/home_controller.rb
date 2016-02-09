3acf450e5da939849ef593c74a8b68b5d1726711
transplanar <glen@glencooney.com>
2016-02-09 00:04:34 -0500

    Start of generator button functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,14 +19,24 @@ class HomeController < ApplicationController
     generate_cards(@slots)
   end
 
-  def generate_cards slots
+  # def generate_cards slots
+  def generate_cards
+    puts "Executing card set generator"
+
+    slots = Slot.all
+    chosen_cards = []
 
     slots.each do |slot|
-      if slot.cards.blank? && slot.sql_prepend.blank?
+      puts "Generating card for slot #{slot.id}"
+
+
+      if slot.cards.blank?
         slot.cards = Card.all
       end
 
       random_card = slot.cards.sample
+
+      puts "Current chosen cards: #{chosen_cards}"
 
       while chosen_cards.include? random_card
         random_card = slot.cards.sample
@@ -35,6 +45,13 @@ class HomeController < ApplicationController
       chosen_cards << random_card
 
       slot.update_attribute(:image_url, random_card.image_url)
+    end
+
+    redirect_to root_path
+
+    respond_to do |format|
+      format.html
+      format.js
     end
   end
 end
