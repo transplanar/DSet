98f4769a4ca86fbd08f4ac49bbf7641825193ea1
transplanar <glen@glencooney.com>
2016-02-14 15:13:35 -0500

    Split search method into multiple smaller methods

--- app/models/card.rb
+++ app/models/card.rb
@@ -11,123 +11,29 @@ class Card < ActiveRecord::Base
 
   # REVIEW split this into separate functions?
   # TODO add optional param to search to allow 'autocomplete' categories
-  def self.search search, slot
-    unless search.blank?
+  def self.search search_str, slot
+    unless search_str.blank?
       columns = get_relevant_columns()
 
-      unless is_numeric?(search)
-        search_queries = search.split(', ')
+      unless is_numeric?(search_str)
+        search_queries = search_str.split(', ')
       else
-        search_queries = [search.to_s]
+        search_queries = [search_str.to_s]
       end
 
-      # Search for matches against first search term
-      query = search_queries.first
-      match_columns = Hash.new
+      sql_hash = generate_sql_hash(search_queries, columns, slot)
 
-      sql_hash = Hash.new
+      results = generate_results_from_sql (sql_hash)
 
-      columns.each do |col|
-        test_search = Card.send( "_#{col}", query )
+      matched_terms = get_matching_terms(search_queries, columns, results)
 
-        unless test_search.blank?
-          unless slot.sql_prepend.blank?
-            sql_hash[col] = slot.sql_prepend + " AND "+ test_search.to_sql.gsub(/.*?(?=\()/im, "")
-          else
-            sql_hash[col] = Card.send( "_#{col}", query).to_sql
-          end
-        end
-      end
-
-      multisearch = search_queries.count > 1
-
-      if multisearch
-        multi_result = Hash.new
-
-        query_2 = search_queries.second
-        match_columns_2 = Hash.new
-
-        # Find columns with matching terms
-        columns.each do |col|
-          unless Card.send( "_#{col}", query_2 ).blank?
-            match_columns_2[col] = query_2
-          end
-        end
-
-        sql_hash.each do |k,v|
-          match_columns_2.each do |col, query|
-            multi_result["#{k} > #{col}"] = v + " AND " + Card.send( "_#{col}", query ).to_sql.gsub(/.*?(?=\()/im, "")
-          end
-        end
-      end
-
-      # Use appropriate hash based on number of chained queries
-      results = Hash.new
-
-      unless multisearch
-        _results = sql_hash
-      else
-        _results = multi_result
-      end
-
-      # Perform search
-      _results.each do |heading, sql|
-        cards = Card.find_by_sql(sql)
-
-        unless cards.blank?
-          results[heading] = cards
-        end
-      end
-
-      # Get the exact terms matched by search
-      matched_terms = Hash.new
-
-      # FIXME this does not display all the correct terms for multi-keyword categories
-      columns.each do |col|
-        results.each do |k, card|
-          card.each do |c|
-            unless col == 'name'
-              if col == "cost"
-                split_terms = [c["#{col}"].to_s]
-              else
-                split_terms = c["#{col}"].split(', ')
-              end
-
-              split_terms.each do |term|
-                search_queries.each do |query|
-                  if term.downcase.include? query.downcase
-                    matched_terms[col] = term
-                  end
-                end
-              end
-            end
-          end
-        end
-      end
-
-      cards_to_slot = []
-
-      results.each do |k, card|
-        card.each do |c|
-          cards_to_slot << c
-        end
-      end
-
-      cards_to_slot.uniq!
-
-      slot.cards = cards_to_slot
-      slot.update_attribute(:queries, search)
+      save_cards_to_slot(search_str, results, slot)
     else
-      # If no search query is given, make everything blank
       unless slot.sql_prepend.blank?
-        cards_to_slot = []
-
         cards = Card.find_by_sql(slot.sql_prepend)
 
-        cards_to_slot.uniq!
-
         slot.cards = cards
-        slot.update_attribute(:queries, search)
+        slot.update_attribute(:queries, search_str)
       else
         slot.cards = Card.all
       end
@@ -148,5 +54,104 @@ class Card < ActiveRecord::Base
 
   def self.is_numeric?(obj)
     obj.to_s.match(/\A[+-]?\d+?(\.\d+)?\Z/) == nil ? false : true
+  end
+
+  def self.generate_sql_hash queries, columns, slot
+    query = queries.first
+
+    results_hash = Hash.new
+    multi_result = Hash.new
+
+    queries.each do |query|
+      if query == queries.first
+        columns.each do |col|
+          test_search = Card.send( "_#{col}", query )
+
+          unless test_search.blank?
+            unless slot.sql_prepend.blank?
+              results_hash[col] = slot.sql_prepend + " AND "+ test_search.to_sql.gsub(/.*?(?=\()/im, "")
+            else
+              results_hash[col] = test_search.to_sql
+            end
+          end
+        end
+      elsif query == queries.second
+        columns.each do |col|
+          results_hash.each do |col_1, sql |
+            test_search = sql + " AND " + Card.send( "_#{col}", query).to_sql.gsub(/.*?(?=\()/im, "")
+
+            unless test_search.blank?
+              multi_result["#{col_1} > #{col}"] = sql + " AND " + Card.send( "_#{col}", query ).to_sql.gsub(/.*?(?=\()/im, "")
+            end
+          end
+        end
+      end
+    end
+
+    unless queries.count > 1
+      _results = results_hash
+    else
+      _results = multi_result
+    end
+    return _results
+  end
+
+  # FIXME this does not display all the correct terms for multi-keyword categories
+
+  def self.get_matching_terms search_queries, columns, results
+    matches = Hash.new
+
+    columns.each do |col|
+      results.each do |k, card|
+        card.each do |c|
+          unless col == 'name'
+            if col == "cost"
+              split_terms = [c["#{col}"].to_s]
+            else
+              split_terms = c["#{col}"].split(', ')
+            end
+
+            split_terms.each do |term|
+              search_queries.each do |query|
+                if term.downcase.include? query.downcase
+                  matches[col] = term
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+
+    return matches
+  end
+
+  def self.generate_results_from_sql sql_hash
+    results = Hash.new
+
+    sql_hash.each do |heading, sql|
+      cards = Card.find_by_sql(sql)
+
+      unless cards.blank?
+        results[heading] = cards
+      end
+    end
+
+    return results
+  end
+
+  def self.save_cards_to_slot search_str, results, slot
+    cards_to_slot = []
+
+    results.each do |k, card|
+      card.each do |c|
+        cards_to_slot << c
+      end
+    end
+
+    cards_to_slot.uniq!
+
+    slot.cards = cards_to_slot
+    slot.update_attribute(:queries, search_str)
   end
 end
