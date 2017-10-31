23db63ebd4464b2bdabdb1dd13c13ef9e5f57234
Glen Cooney <glen@glencooney.com>
2017-10-31 01:39:37 +0000

    Chained queries WIP

--- app/models/card.rb
+++ app/models/card.rb
@@ -20,9 +20,8 @@ class Card < ActiveRecord::Base
     queries_array = queries_to_array(queries_string)
     subqueries = format_for_regex(queries_array)
     matches = get_matches(subqueries)
-    # convert_to_match_hash(matches)
-# (query, match_data, columns = [])
-    # get_card_subset(subqueries.first, [], [])
+    
+    # matches.group_by{|k,v| v[:columns]}
   end
 
   def self.queries_to_array(queries_string)
@@ -61,117 +60,63 @@ class Card < ActiveRecord::Base
     subqueries.each do |query|
       match_data =
         get_card_subset(query, match_data)
-
-      # columns -= matched_categories unless query == subqueries.last
     end
 
-    return [] if match_data.empty?
+    return {} if match_data.empty?
 
-    match_data.group_by { |elem| elem[:columns] }.sort
+    matches.group_by{|k,v| v[:columns]}
   end
 
   # Match data is a hash of query, card, column
 
   private_class_method def self.get_card_subset(query, match_data)
-    # card_set = (match_data.empty? ? Card.all : to_active_record(match_data))
     if(match_data.empty?)
       card_set = Card.all
       matched_columns = []
     else
       card_set = Card.where(name: match_data.keys)
-      matched_columns = match_data.pluck(:columns)
+      matched_columns = match_data.map{|k,v| v[:columns]}.flatten.uniq
     end
-    
-    card_set = (match_data.empty? ? Card.all : Card.where(name: match_data.keys))
-    # columns = []
-    matched_card_ids = card_set.pluck(:id)
-    
+
     if(numeric?(query))
       matched_cards = card_set.where(cost: query)
 
-      # TODO move this to a separate method
       matched_cards.each do |card|
+        p "Populating card #{card}"
         if (match_data[card.name])
+          p "Existing record found as #{match_data[card.name]}"
+          p "#{match_data[card.name][:columns]}"
           match_data[card.name][:columns] << 'cost'
           match_data[card.name][:queries] << query
+          p "Existing record COMPLETE"
         else
+          p "NEW record"
           match_data[card.name] = {} 
           match_data[card.name][:columns] = ['cost']
           match_data[card.name][:queries] = [query]
+          p "NEW record COMPLETE"
         end
       end
-      
-      p "NUMERIC"
-      p "RAW MATCH DATA #{match_data}"
-      p "KEYS #{match_data.keys}"
     else
-      # NOTE: Must associate each keyword/term/category match as single data object
-      # Field being compared against
-      # matched_columns = match_data.empty? ? [] : match_data.pluck(:columns)
-      # card_subset = Card.where(name: match_data.keys)
-      # matched_card_ids = match_data.pluck(:card)
-      
       keyword_set = CardKeyword.where(CardKeyword.arel_table[:card_id].in card_set.pluck(:id))
                               .where(CardKeyword.arel_table[:category].not_in matched_columns)
                               .distinct
       
       keyword_matches = keyword_set.where('name ILIKE ?', query).distinct
-      # matched_cards = keyword_matches.pluck(:card)
       
       keyword_matches.each do |kw|
         if (match_data[kw.card.name])
           match_data[kw.card.name][:columns] << kw.category
-          # match_data[kw.card.name][:queries] << query
           match_data[kw.card.name][:terms] << kw.name
         else
           match_data[kw.card.name] = {} 
           match_data[kw.card.name][:columns] = [kw.category]
-          # match_data[kw.card.name][:queries] = [query]
           match_data[kw.card.name][:terms] = [kw.name]
         end
       end
-      
-      p "ALPHA"
-      p "RAW MATCH DATA #{match_data}"
-      p "KEYS #{match_data.keys}"
-      
-      # matched_cards.each do |card|
-      #   if (match_data[card.name])
-      #     match_data[card.name][:columns] << 'cost'
-      #     match_data[card.name][:queries] << query
-      #   else
-      #     match_data[card.name] = {} 
-      #     match_data[card.name][:columns] = ['cost']
-      #     match_data[card.name][:queries] = [query]
-      #   end
-      # end
-      
-      # matched_categories = keyword_matches.pluck(:category).uniq!
-      # card_ids = keyword_matches.pluck(:card_id)
-      # matched_cards = Card.where(Card.arel_table[:id].in card_ids)
-      
-      # p "---CARDS #{matched_cards.pluck(:name)}"    
-      # p "---KEYWORDS #{keyword_matches.pluck(:name)}"    
-
-      # keyword_matches = CardKeyword.where('name ILIKE ?', query).distinct
-      
-      # Array of categories already matched (to prevent duplicates)
-      
-      
-      # keyword_matches.each do |keyword|
-      #   card = Card.where(id: keyword.card_id)
-        
-        
-      # end
-      
-      # card_ids = keyword_matches.pluck(:card_id)
-      # matched_cards = card_set.where(id: card_ids)
-      
-      # match_data.empty? new_result(matched_categories, matched_cards) 
-      #                   : update_result(matched_categories, matched_cards);
     end
-
-    [results, matched_categories]
+    
+    match_data
   end
   
   # private_class_method def self.match_data_to_active_record(data)
