44ce4ed75937a46413f398fbb4f496212b2bc806
Glen Cooney <glen@glencooney.com>
2018-03-14 01:27:27 -0700

    Code cleanup

--- app/models/card.rb
+++ app/models/card.rb
@@ -8,6 +8,8 @@ class Card < ActiveRecord::Base
   has_and_belongs_to_many :slots
   has_many :card_keywords
 
+  REGEX_DELIMITER = '.*'.freeze
+
   # TODO: fix to assign to slot
   def self.search(queries_string, slot)
     return [] if queries_string.blank?
@@ -17,32 +19,29 @@ class Card < ActiveRecord::Base
     matches = get_matches(formatted_queries)
 
     matches.group_by { |_, v| v[:columns] }
+    # TODO: Restore save to slot functionality
   end
 
   private_class_method def self.format_multi_char_queries(arr)
     result = []
 
     arr.each do |elem|
-      if numeric?(elem)
-        result << elem
-      else
-        result << format_string_query(elem)
-      end
+      result << (numeric?(elem) ? elem : format_string_query(elem))
     end
 
-    return result
+    result
   end
 
   private_class_method def self.format_string_query(query)
-    return query if (query.length < 2)
+    return query if query.length < 2
 
-    formatted_query = '.*'
+    formatted_query = REGEX_DELIMITER
 
     query.split('').each do |ch|
-      formatted_query += (ch + ".*")
+      formatted_query += (ch + REGEX_DELIMITER)
     end
 
-    return formatted_query
+    formatted_query
   end
 
   private_class_method def self.get_matches(subqueries)
@@ -51,29 +50,33 @@ class Card < ActiveRecord::Base
     subqueries.each do |query|
       result = get_card_subset(query, match_data)
 
-      match_data = result == nil ? match_data : result
+      match_data = result.nil? ? match_data : result
     end
 
     match_data
   end
 
-  # Match data is a hash of query, card, column
-
-  private_class_method def self.get_card_subset(query, match_data)
-  # TODO needs new_match_data, cull matches that do not match ALL queries
-  # only aggregate fields that need to carry over (columns)
-    new_match_data = {}
-
+  private_class_method def self.get_card_set(match_data)
     if(match_data.empty?)
       card_set = Card.all
       matched_columns = []
     else
       card_set = Card.where(name: match_data.keys)
       matched_columns = match_data.map{ |_, v| v[:columns] }.flatten.uniq
-      matched_terms = match_data.map{ |_, v| v[:terms] }.flatten.uniq
+      # TODO: Do something with matched terms
+      # matched_terms = match_data.map{ |_, v| v[:terms] }.flatten.uniq
     end
 
-    if(numeric?(query))
+    [card_set, matched_columns]
+  end
+
+  # TODO: Rename methods for readibility
+  private_class_method def self.get_card_subset(query, match_data)
+    new_match_data = {}
+
+    card_set, matched_columns = get_card_set(match_data)
+
+    if numeric?(query)
       matched_cards = card_set.where(cost: query)
 
       matched_cards.each do |card|
@@ -105,39 +108,39 @@ class Card < ActiveRecord::Base
       end
     end
 
-    return new_match_data
+    new_match_data
   end
 
   private_class_method def self.query_to_regex(query)
     '/' + query.gsub(/[\[\]]/, '') + '/i'
   end
 
-    # FIXME move to helper?
+  # FIXME: move to helper?
   private_class_method def self.relevant_columns
     matched_categories = %w[id image_url created_at updated_at slot_id]
     Card.attribute_names - matched_categories
   end
 
-# FIXME move to helper?
+  # FIXME: move to helper?
   private_class_method def self.numeric?(str)
-    str.to_s.delete('%').match(/\A[+-]?\d+?(\.\d+)?\Z/) != nil
+    str.to_s.delete(REGEX_DELIMITER).match(/\A[+-]?\d+?(\.\d+)?\Z/) != nil
   end
 
-# FIXME move to helper?
+  # FIXME move to helper?
   private_class_method def self.merge_match_data(match_data, new_match_data, card, column, query)
     new_match_data[card.name] = {}
     new_match_data[card.name][:card] = card
-    new_match_data[card.name][:columns] = merge_result_hash(match_data, card.name,:columns, column)
-    new_match_data[card.name][:terms] = merge_result_hash(match_data, card.name, :terms, query)
 
-    new_match_data
+    new_columns = merge_result_hash(match_data, card.name, :columns, column)
+    new_match_data[card.name][:columns] = new_columns
+
+    new_terms = merge_result_hash(match_data, card.name, :terms, query)
+    new_match_data[card.name][:terms] = new_terms
   end
 
   # FIXME: move to helper?
   private_class_method def self.merge_result_hash(hsh, key, sub_key, new_elem)
-    return [new_elem] if hsh[key].nil?
-
-    return [new_elem] if hsh[key][sub_key].nil?
+    return [new_elem] if hsh[key].nil? || hsh[key][sub_key].nil?
 
     exists_in_hsh = hsh[key][sub_key].include?(new_elem)
 
